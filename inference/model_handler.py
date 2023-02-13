b8ed7d1535e1a929433b41fc909dcd5b95d36631
ayush <ayushagarwal1898@gmail.com>
2023-02-13 21:36:50 +0530

    Tested Inference Scripts in local

--- inference/model_handler.py
+++ inference/model_handler.py
@@ -8,6 +8,8 @@ from loguru import logger
 from ultralytics import YOLO
 import boto3
 
+curr_dir = os.path.abspath(os.path.dirname(__file__))
+
 class ModelHandler(object):
     """
     A sample Model handler implementation.
@@ -31,6 +33,8 @@ class ModelHandler(object):
         logger.info(f"Model Loaded")
 
     def download_image(self, s3_path, local_path, bucket_name):
+        if os.path.exists(s3_path):
+            return s3_path
         s3 = boto3.resource('s3')
         my_bucket = s3.Bucket(bucket_name)
         try:
@@ -39,6 +43,7 @@ class ModelHandler(object):
         except Exception as e:
             logger.error(f"Error downloading {s3_path} to {local_path}")
             raise Exception(e)
+        return local_path
 
     def format_output(self, model_output):
         model_output = model_output[0].boxes 
@@ -51,7 +56,7 @@ class ModelHandler(object):
                 "coordinates" : {
                     "x" : box[0], "y" : box[1], "w" : box[2], "h" : box[3]
                 },
-                "class_id" : class_id,
+                "class_id" : int(class_id),
                 "confidence" : conf 
             }
             result_list.append(tmp_dict)
@@ -64,13 +69,20 @@ class ModelHandler(object):
             device='cpu'
         logger.info(f"Running Inference")
         image_dict = json.loads(data[0].get('body'))
-        # Download file 
-        # download_image()
-        image_path = None
+        image_path = image_dict.get('image_path')
+        bucket_name = image_dict.get('bucket_name')
+        conf = image_dict.get('conf')
+
+        local_path = os.path.join(curr_dir, os.path.basename(image_path))
+        local_path = self.download_image(s3_path=image_path, local_path=local_path, bucket_name=bucket_name)
+
+        self.model_dict['params']['conf'] = conf
+        self.model_dict['params']['device'] = device
         model_output = self.model.predict(image_path, **self.model_dict.get('params'))
         model_output = self.format_output(model_output)
-
         image_dict['results'] = model_output
+
+        os.remove(local_path)
         return image_dict
     
 _service = ModelHandler()
@@ -88,3 +100,8 @@ def handle(data, context):
     out = _service.handle(data, context)
     out['is_initalized'] = is_initialized
     return [out]
+
+if __name__ == '__main__':
+    import json
+    payload = json.load(open('sample_files/payload.json'))
+    print(handle(payload, context={}))
--- training/train.py
+++ training/train.py
@@ -7,7 +7,9 @@ import argparse
 import json 
 from loguru import logger 
 from ultralytics import YOLO
-
+import shutil 
+import xtarfile as tarfile 
+import boto3
 
 from prepare_dataset import prepare_dataset
 
@@ -28,12 +30,56 @@ def parse_args():
     return args
 
 
+def upload_file_on_s3(local_path, s3_path, bucket_name):
+    my_bucket = boto3.resource('s3').Bucket(bucket_name)
+    try:
+        my_bucket.upload_file(local_path, s3_path)
+        logger.info(f"Uploaded {local_path} to {s3_path}")
+    except Exception as e:
+        logger.error(f"Error uploading in {local_path} to {s3_path}")
+        raise Exception(e)
+
+
+
 def train(data_config, model_config):
     model_config['data'] = data_config['yaml_file_path']
     model = YOLO(model_config.get('model','yolov8n.pt'))
     model.train(**model_config)
 
-def main(aws_config, data_config, model_config):
+def deploy_model(inference_config, deploy_config, model_dir, model_path, bucket_name):
+    model_name = deploy_config.get('model_name')
+    deploy_dir = deploy_config.get('deploy_dir')
+
+    model_dir = os.path.abspath(model_dir)
+    os.makedirs(model_dir,exist_ok=True)
+    
+    assert os.path.exists(model_path), f"{model_path} does not exists"
+    shutil.copy(model_path, model_dir)
+
+    """
+    Use of inference configuration is to demonstrate how we can use different pre-defined parameters
+    for any custom inference pipeline. For example, the following paramters are available here:
+         "iou": 0.7,
+        "augment": true
+    More parameters are available here: https://docs.ultralytics.com/cfg/
+     """
+    inference_config['weights'] = os.path.basename(model_path)
+    with open(os.path.join(model_dir,'model.json'),'w') as f:
+        json.dump(inference_config, f)
+
+    save_dir = os.path.dirname(model_path)
+    tar_file_path = os.path.join(save_dir,f'{model_name}.tar.gz')
+    with tarfile.open(tar_file_path, 'w:gz') as tar:
+        tar.add(model_dir,arcname='.')
+
+    upload_file_on_s3(
+        local_path=tar_file_path,
+        s3_path=os.path.join(deploy_dir,f"{model_name}.tar.gz"),
+        bucket_name=bucket_name)
+
+
+
+def main(aws_config, data_config, model_config, inference_config, deploy_config):
     # Step 1 : Prepare dataset
     logger.info('Preparing dataset for Model Training')
     prepare_dataset(data_config=data_config, aws_config=aws_config)
@@ -41,6 +87,17 @@ def main(aws_config, data_config, model_config):
     #Step 2 : Format model_config and train the model
     logger.info('Training Model')
     train(data_config, model_config)
+
+    #Step3 : Upload Model Artifacts on s3
+    logger.info(f"Deploying model artifacts on s3")
+    model_path = os.path.abspath('runs/detect/train/weights/best.pt')
+    deploy_model(
+        inference_config=inference_config,
+        deploy_config=deploy_config,
+        model_dir=data_config.get('model_dir'),
+        model_path=model_path,
+        bucket_name=aws_config.get('bucket_name')
+    )
 
 
 if __name__ == '__main__':
@@ -53,5 +110,8 @@ if __name__ == '__main__':
     data_config = config.get('data_config')
     model_config = config.get('model_config')
     aws_config = config.get('aws_config')
+    deploy_config = config.get('deploy_config')
+    inference_config = config.get('inference_config')
 
-    main(aws_config, data_config, model_config)
+    main(aws_config, data_config, model_config, inference_config, deploy_config)
+
